b2ab6a6c6d77c460fc740d702f25e5bbbf0fa925
Yusup Firmansyah <yhusup6@gmail.com>
2022-05-18 10:16:49 +0700

    Theme Value

--- yusup_flutix/lib/shared/shared.dart
+++ yusup_flutix/lib/shared/shared.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:yusup_flutix/bloc/blocs.dart';
 
 part 'shared_value.dart';
+part 'theme.dart';
