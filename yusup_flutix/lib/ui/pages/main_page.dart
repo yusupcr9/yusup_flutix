c0f3d377b3fdfc10116c9afb2d4e683fe2b9647d
Yusup Firmansyah <yhusup6@gmail.com>
2022-05-19 10:55:19 +0700

    Main Page Navigation

--- yusup_flutix/lib/ui/pages/main_page.dart
+++ yusup_flutix/lib/ui/pages/main_page.dart
@@ -8,6 +8,15 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int? bottomNavBarIndex;
+  PageController? pageController;
+  @override
+  void initState() {
+    super.initState();
+    bottomNavBarIndex = 0;
+    pageController = PageController(initialPage: bottomNavBarIndex ?? 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +29,22 @@ class _MainPageState extends State<MainPage> {
               child: Container(
             color: Color(0xFFF6F7F9),
           )),
-          ListView(),
+          PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              setState(() {
+                bottomNavBarIndex = index;
+              });
+            },
+            children: [
+              Center(
+                child: Text("New Movie"),
+              ),
+              Center(
+                child: Text("My Tickets"),
+              ),
+            ],
+          ),
           createCustomBottomNavBar(),
           Align(
             alignment: Alignment.bottomCenter,
@@ -59,6 +83,41 @@ class _MainPageState extends State<MainPage> {
               color: Colors.white,
               borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+          child: BottomNavigationBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            selectedItemColor: mainColor,
+            unselectedItemColor: Color(0xFFE5E5E5),
+            currentIndex: bottomNavBarIndex ?? 0,
+            onTap: (index) {
+              setState(() {
+                bottomNavBarIndex = index;
+                pageController?.jumpToPage(index);
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                label: "New Movies",
+                icon: Container(
+                  margin: EdgeInsets.only(bottom: 6),
+                  height: 20,
+                  child: Image.asset((bottomNavBarIndex == 0)
+                      ? "assets/ic_movie.png"
+                      : "assets/ic_movie_grey.png"),
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "My Tickets",
+                icon: Container(
+                  margin: EdgeInsets.only(bottom: 6),
+                  height: 20,
+                  child: Image.asset((bottomNavBarIndex == 1)
+                      ? "assets/ic_tickets.png"
+                      : "assets/ic_tickets_grey.png"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- yusup_flutix/lib/ui/pages/pages.dart
+++ yusup_flutix/lib/ui/pages/pages.dart
@@ -3,6 +3,7 @@ import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:yusup_flutix/bloc/blocs.dart';
